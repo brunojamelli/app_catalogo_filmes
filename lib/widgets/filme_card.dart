17756a1210a1bd17de0f3fd690dc697ef65b4e1c
brunojamelli <brunojamelli09@gmail.com>
2025-03-05 18:13:48 -0300

    feat: ajuste no alinhamento das informações no widget de card de filme

--- lib/widgets/filme_card.dart
+++ lib/widgets/filme_card.dart
@@ -23,7 +23,8 @@ class FilmeCard extends StatelessWidget {
         child: Padding(
           padding: EdgeInsets.all(8.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center, // Centraliza horizontalmente
+            mainAxisAlignment: MainAxisAlignment.center, // Centraliza verticalmente
             children: [
               Expanded(
                 child: Image.network(
@@ -40,34 +41,41 @@ class FilmeCard extends StatelessWidget {
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                 maxLines: 1,
                 overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center, // Centraliza o texto
               ),
               SizedBox(height: 5),
-              Text('Ano: ${filme.ano}'),
+              Text(
+                'Ano: ${filme.ano}',
+                textAlign: TextAlign.center, // Centraliza o texto
+              ),
               SizedBox(height: 5),
-              Text('Direção: ${filme.direcao}'),
+              Text(
+                'Direção: ${filme.direcao}',
+                textAlign: TextAlign.center, // Centraliza o texto
+              ),
               SizedBox(height: 5),
               Text(
                 'Resumo: ${filme.resumo}',
                 maxLines: 2,
                 overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center, // Centraliza o texto
               ),
               SizedBox(height: 5),
-              // Text('Nota: ${filme.nota}'),
               RatingBar.builder(
-                      initialRating: filme.nota ?? 0.0,
-                      minRating: 0,
-                      direction: Axis.horizontal,
-                      allowHalfRating: true,
-                      itemCount: 5,
-                      itemSize: 20,
-                      itemBuilder: (context, _) => Icon(
-                        Icons.star,
-                        color: Colors.amber,
-                      ),
-                      onRatingUpdate: (rating) {
-                        print("Nova avaliação: $rating");
-                      },
-                      ignoreGestures: true,
+                initialRating: filme.nota ?? 0.0,
+                minRating: 0,
+                direction: Axis.horizontal,
+                allowHalfRating: true,
+                itemCount: 5,
+                itemSize: 20,
+                itemBuilder: (context, _) => Icon(
+                  Icons.star,
+                  color: Colors.amber,
+                ),
+                onRatingUpdate: (rating) {
+                  print("Nova avaliação: $rating");
+                },
+                ignoreGestures: true,
               ),
             ],
           ),
